3a1bf13e5b468a49fe080639e032b366e2d63c17
Juha Ruistola <ruistola@gmail.com>
2024-03-26 16:38:38 +0200

    Some initial tests with an actual DB connection

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,34 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
 app.use(cors());
-
 app.use(express.json());
+
+console.log(`DB at ${process.env.DB}`);
+
+mongoose.connect(process.env.DB, {})
+    .then(() => console.log("Connected to DB"))
+    .catch((err) => console.log(err));
+
+mongoose.Promise = global.Promise;
+
+const schema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+    },
+});
+
+console.log(mongoose.modelNames());
 
 app.get("/", (_req, res) => res.status(200).json({ message: "API is online" }));
 

1f2ecfab09a042e1ecc6430a7cec9642e2e85487
Juha Ruistola <ruistola@gmail.com>
2024-04-02 14:30:06 +0300

    Added a SDF raymarcher

--- frontend/src/Canvas.tsx
+++ frontend/src/Canvas.tsx
@@ -12,6 +12,20 @@ export default function Canvas({ todos }: Props) {
   const [ctx, setCtx] = useState<CanvasRenderingContext2D | null>(null);
   const [glctx, setGlctx] = useState<WebGL2RenderingContext | null>(null);
 
+  const generateTodoPositions = (todos: Todo[]): Float32Array => {
+    const result = new Float32Array(todos.length*3);
+    for (let i=0; i<todos.length; ++i) {
+      result[i*3] = -5 + 10*(i/todos.length);
+      result[i*3+1] = 0;
+      result[i*3+2] = 0;
+    }
+    return result;
+  };
+
+  const getTodos = () => {
+    return generateTodoPositions(todos);
+  };
+
   const handleMouseMove = (e: MouseEvent ) => {
     if (!(e.target instanceof HTMLElement)) return;
     let rect = (e.target as HTMLElement).getBoundingClientRect();
@@ -36,9 +50,11 @@ export default function Canvas({ todos }: Props) {
   useLayoutEffect(() => {
     if (!canvasRef.current) return;
     setGlctx(canvasRef.current.getContext("webgl2"));
-    if (!glctx) return;
-    renderer(glctx);
   }, [canvasRef.current]);
+
+  useLayoutEffect(() => {
+    if (glctx) renderer(glctx, getTodos);
+  }, [glctx, todos]);
 
   return (
     <canvas ref={canvasRef} width="640px" height="480px" style={{ border: "2px solid black", display: "inline-flex", width: "100%", height: "100%" }} onDoubleClick={handleMouseDblClick} />
--- frontend/src/renderer.ts
+++ frontend/src/renderer.ts
@@ -1,4 +1,4 @@
-const renderer = (glctx: WebGL2RenderingContext) => {
+const renderer = (glctx: WebGL2RenderingContext, getTodos: () => Float32Array) => {
 
   const vertexShaderSource =
     `#version 300 es
@@ -15,27 +15,61 @@ const renderer = (glctx: WebGL2RenderingContext) => {
     layout(std140, column_major) uniform;
 
     uniform vec4 res;
-    //uniform vec4 itemPositions[1024];
+    uniform vec3 itemPositions[32];
     uniform int numItems;
 
-    uniform _uData
-    {
-        vec4 res;
-        vec4 bpos;
-        vec4 camX;
-        vec4 camY;
-        vec4 camZ;
-        vec4 camPos;
-        vec4 sliders;
-        vec4 objPos;
-    } uData;
+    // uniform _uData
+    // {
+    //     vec4 res;
+    //     vec4 bpos;
+    //     vec4 camX;
+    //     vec4 camY;
+    //     vec4 camZ;
+    //     vec4 camPos;
+    //     vec4 sliders;
+    //     vec4 objPos;
+    // } uData;
 
     out vec4 color;
 
+    float sphere(in vec3 point, in vec4 sphere)
+    {
+        return length( point - sphere.xyz ) - sphere.w;
+    }
+
+    float f(in vec3 p) {
+      float d = 10000.0;
+      for (int i=0; i<numItems; ++i) {
+        d = min(d, sphere(p, vec4(itemPositions[i], 0.5)));
+      }
+      return d;
+    }
+
     void main()
     {
-      vec2 uv = vec2(gl_FragCoord.x / res.x, gl_FragCoord.y / res.y);
-      color = vec4(uv.x,uv.y,0.0,1.0);
+      float viewPortRatio = res.x / res.y;
+      vec3 clipSpacePixel = vec3(viewPortRatio * (-1.0 + gl_FragCoord.x / res.x * 2.0) , -1.0 + gl_FragCoord.y / res.y * 2.0, 0.0);
+      const float focalDistance = 2.0;
+      vec3 camPos = vec3(0.0, 0.0, 10.0);
+      vec3 camX = vec3(1.0,0.0,0.0);
+      vec3 camY = vec3(0.0,1.0,0.0);
+      vec3 camZ = vec3(0.0,0.0,1.0);
+      vec3 rd = normalize(vec3(camPos - camZ * focalDistance + camX * clipSpacePixel.x + camY * clipSpacePixel.y ) - camPos);
+      vec3 ro = camPos;
+
+      float epsilon = 0.0001;
+      float t = 0.0;
+      color = vec4(0.0,0.0,1.0,1.0);
+      float steps = 0.0;
+      for (int i=0;i<512;++i)
+      {
+          if ( t > 1000.0 ) break;
+          float d = f(ro+rd*t);
+          if (d<epsilon) break;
+          t += d;
+          steps += 1.0;
+      }
+      color = vec4( vec3( 1.0 - ( steps + 1.0 ) / 33.0 ), 1.0 );
     }`;
 
   const createShader = (vsrc: string, fsrc: string) : WebGLProgram | null => {
@@ -78,16 +112,14 @@ const renderer = (glctx: WebGL2RenderingContext) => {
   glctx.bindBuffer( glctx.ELEMENT_ARRAY_BUFFER, ibo );
   glctx.bufferData( glctx.ELEMENT_ARRAY_BUFFER, new Uint16Array( [ 0, 1, 2 ] ), glctx.STATIC_DRAW );
 
-  // TODO: Use the UBO to pass in todo item positions
-  const ubo = glctx.createBuffer();
-  const uData = new Float32Array( 32 );
-
   const width = glctx.drawingBufferWidth;
   const height = glctx.drawingBufferHeight;
 
   glctx.clearColor( 0.0, 0.0, 0.0, 1.0 );
 
   const resLocation = glctx.getUniformLocation(shader, "res");
+  const numItemsLocation = glctx.getUniformLocation(shader, "numItems");
+  const itemPositionsLocation = glctx.getUniformLocation(shader, "itemPositions");
 
   const render = () => {
     glctx.useProgram( shader );
@@ -99,11 +131,9 @@ const renderer = (glctx: WebGL2RenderingContext) => {
     glctx.viewport(0, 0, width, height);
     glctx.uniform4f(resLocation, width, height, 0.0, 0.0);
 
-    glctx.bindBuffer( glctx.UNIFORM_BUFFER, ubo );
-    glctx.bufferData( glctx.UNIFORM_BUFFER, uData , glctx.DYNAMIC_DRAW );
-    glctx.bindBuffer( glctx.UNIFORM_BUFFER, null );
-    glctx.bindBufferBase( glctx.UNIFORM_BUFFER, 0, ubo );
-
+    const todos = getTodos();
+    glctx.uniform1i(numItemsLocation, todos.length/3);
+    glctx.uniform3fv(itemPositionsLocation, todos);
     glctx.drawElements( glctx.TRIANGLES, 3, glctx.UNSIGNED_SHORT, 0 );
 
     glctx.useProgram( null );
